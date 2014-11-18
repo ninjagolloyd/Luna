794475f5545476d93ef9fe571c97d7fb1ddf1f6a
Yannick <contact@studio384.be>
2014-11-18 21:22:23 +0100

    #881 Ho gosh! We're done with the design!

---
+++ install/success.php
@@ -0,0 +1,38 @@
+<?php
+
+session_start();
+
+if(!isset($_SESSION['luna_install_essential']))
+	die('Installation access denied.');
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Install Luna</title>
+        <link href="../include/css/bootstrap.min.css" rel="stylesheet">
+        <link href="install.css" rel="stylesheet">
+    </head>
+    <body class="ready">
+        <div class="site-wrapper">
+            <div class="site-wrapper-inner">
+                <div class="cover-container">
+                    <div class="masthead clearfix">
+						<h3 class="masthead-brand"><span class="luna-brand">Luna</span>Preview</h3>
+                    </div>
+                    <div class="inner cover">
+                        <h1 class="cover-heading">Alright, we're ready to go!</h1>
+                        <p class="lead">
+                            <a href="../index.php" class="btn btn-lg btn-default">Have fun</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <script src="../include/js/jquery.js"></script>
+        <script src="../include/js/bootstrap.min.js"></script>
+    </body>
+</html>
