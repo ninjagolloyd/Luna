8e63a593df0ee1b3b115e33cbfdc0d56847bc6d4
Yannick <contact@studio384.be>
2014-11-18 20:58:07 +0100

    #881 Start of the setup form

---
+++ install/setup.php
@@ -0,0 +1,88 @@
+<?php
+
+session_start();
+
+if(!isset($_SESSION['luna_install_terms']))
+	die('Installation access denied.');
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Install Luna</title>
+        <link href="../include/css/bootstrap.min.css" rel="stylesheet">
+        <link href="install.css" rel="stylesheet">
+    </head>
+    <body class="default">
+        <div class="site-wrapper">
+            <div class="site-wrapper-inner">
+                <div class="cover-container">
+                    <div class="masthead clearfix">
+						<h3 class="masthead-brand"><span class="luna-brand">Luna</span>Initialization</h3>
+                    </div>
+                    <div class="inner cover">
+						<form class="form-horizontal" method="post" action="setup.php">
+							<div class="panel panel-default">
+								<div class="panel-body">
+									<fieldset>
+										<div class="form-group">
+											<label class="col-sm-3 control-label">Database<span class="help-block">What database do you want to use?</span></label>
+											<div class="col-sm-9">
+												<select class="form-control" name="req_db_type">
+													<option value="1" selected="selected">MySQL</option>
+													<option value="2" selected="selected">MySQLi</option>
+													<option value="3" selected="selected">MySQL InnoDB</option>
+													<option value="4" selected="selected">MySQLi InnoDB</option>
+													<option value="5" selected="selected">SQLite</option>
+													<option value="6" selected="selected">PostgreSQL</option>
+												</select>
+											</div>
+										</div>
+										<div class="form-group">
+											<label class="col-sm-3 control-label">Server hostname<span class="help-block">Where's the server?</span></label>
+											<div class="col-sm-9">
+												<input type="text" class="form-control" name="req_db_host" value="" />
+											</div>
+										</div>
+										<div class="form-group">
+											<label class="col-sm-3 control-label">Name<span class="help-block">The database name</span></label>
+											<div class="col-sm-9">
+												<input id="req_db_name" type="text" class="form-control" name="req_db_name" value="" />
+											</div>
+										</div>
+										<div class="form-group">
+											<label class="col-sm-3 control-label">Username<span class="help-block">Your database username</span></label>
+											<div class="col-sm-9">
+												<input type="text" class="form-control" name="db_username" value="" />
+											</div>
+										</div>
+										<div class="form-group">
+											<label class="col-sm-3 control-label">Password</label>
+											<div class="col-sm-9">
+												<input type="password" class="form-control" name="db_password" />
+											</div>
+										</div>
+										<div class="form-group">
+											<label class="col-sm-3 control-label">Prefix<span class="help-block">Set for more Luna installation in this database</span></label>
+											<div class="col-sm-9">
+												<input id="db_prefix" type="text" class="form-control" name="db_prefix" value="" />
+											</div>
+										</div>
+									</fieldset>
+								</div>
+							</div>
+							<p class="lead">
+								<a href="setup.php" class="btn btn-lg btn-default">Install Luna</a>
+							</p>
+						</form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <script src="../include/js/jquery.js"></script>
+        <script src="../include/js/bootstrap.min.js"></script>
+    </body>
+</html>
